d87763bc34aecdbbcbe6eb151475d6f2386131a0
sulaimanadegoke <sulaimanogundare@gmail.com>
2024-05-14 20:45:51 +0100

    Initial commit

---
+++ FizzBuzz.SO/FizzBuzz.SO/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzz.SO
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            foreach (int number in numbers)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
