fd37b687cb138e55443dce3fb29e5894fc28776f
sulaimanadegoke <sulaimanogundare@gmail.com>
2024-05-28 23:40:17 +0100

    updated assignment

--- SortingOutAnArrayOfIntegersInAscendingOrder.SO/SortingOutAnArrayOfIntegersInAscendingOrder.SO/Program.cs
+++ SortingOutAnArrayOfIntegersInAscendingOrder.SO/SortingOutAnArrayOfIntegersInAscendingOrder.SO/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 59, 30, 25, 10, 22, 11, 70 };
+            int[] sortedArray = BubbleSort(array);
+            Console.WriteLine(string.Join(", ", sortedArray));
+        }
+
+        public static int[] BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap array[j] and array[j + 1]
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+
         }
     }
 }
